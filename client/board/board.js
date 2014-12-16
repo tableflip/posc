e4c6c32e811390921d7a9866627780e15f0e926b
Liam Wooding <liamwooding@gmail.com>
2014-12-16 11:32:05 +0000

    Fixed some bugs in the card-shuffling behaviour

--- client/board/board.js
+++ client/board/board.js
@@ -43,33 +43,43 @@ Template.board.rendered = function () {
     var $slotEl = $(slotEl)
     var cardId = $(cardEl).data('card')
     var card = Cards.findOne(cardId)
-    var slot = $slotEl.data('slot')
+    var slot = parseInt($slotEl.attr('data-slot'))
     var toBucketId = $slotEl.parents('.bucket').data('bucket')
 
-    console.log(cardId, 'to', toBucketId, slot)
+    //console.log(cardId, 'to', toBucketId, slot)
+
+    console.log($slotEl.attr('data-slot'), card.preferredSlot, 'to', slot)
 
     if ($slotEl.find('.card').length === 0) {
       // slot is empty, so simples
+      console.log('empty slot')
       return Cards.update(cardId, { $set: {bucket: toBucketId, preferredSlot: slot} })
     }
     
     // we're dropping onto a card, so gonna have to dance.
     var cardInTheWay = $slotEl.find('.card').data('card')
     
-    if (cardInTheWay === cardId) return // We'll just leave this where we found it.
+    if (cardInTheWay === cardId) {
+      console.log('no bother')
+      return // We'll just leave this where we found it.
+    }
 
-    if (!isFull(toBucketId)) {
+    if (cardInTheWay && (slot === card.preferredSlot - 1 || slot === card.preferredSlot + 1)) {
+      // Swapsies
+      console.log('swapsies!')
+      swapCards(card, Cards.findOne(cardInTheWay), Buckets.findOne(toBucketId))
+    } else if (!isFull(toBucketId)) {
+      console.log('nudging right')
       // there's room in this bucket so let's jiggle
       nudgeRight(Cards.findOne(cardInTheWay), Buckets.findOne(toBucketId))
+      Cards.update(cardId, { $set:{bucket:toBucketId, preferredSlot:slot}})
       
     } else {
       console.log('is full')
       // no more room in the bucket so let's swap places
       Cards.update(cardInTheWay, { $set: {bucket: card.bucket, preferredSlot:card.preferredSlot}})
-    }
-    
-    // room has been made, so now let's update the dragged card
-    Cards.update(cardId, { $set:{bucket:toBucketId, preferredSlot:slot}})
+      Cards.update(cardId, { $set:{bucket:toBucketId, preferredSlot:slot}})
+    }    
   })
 }
 
@@ -85,6 +95,20 @@ function nudgeRight(card, bucket) {
   var nextCard = Cards.findOne({bucket: bucket._id, preferredSlot: nextSlot})
   nudgeRight(nextCard, bucket)
   Cards.update(card._id, { $set: {preferredSlot: nextSlot}})
+}
+
+function nudgeLeft(card, bucket) {
+  if (!card) return;
+  var previousSlot = (card.preferredSlot - 1) % bucket.limit
+  var previousCard = Cards.findOne({bucket: bucket._id, preferredSlot: previousSlot})
+  nudgeLeft(previousCard, bucket)
+  Cards.update(card._id, { $set: {preferredSlot: previousSlot}})
+}
+
+function swapCards(card1, card2, bucket) {
+  if (!card1 || !card2) return;
+  Cards.update(card1._id, { $set: {preferredSlot: card2.preferredSlot}})
+  Cards.update(card2._id, { $set: {preferredSlot: card1.preferredSlot}})
 }
 
 Template.bucket.helpers({
